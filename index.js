4582f8d844b910085a77a9392fb8681886c12e55
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2023-01-27 19:33:49 +0000

    inquire function and template literal draft added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// fs is a Node standard library package for reading and writing files
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
@@ -5,7 +6,31 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: 'What is your location?',
+        name: 'location',
+      },
+      {
+        type: 'input',
+        message: 'Write a short bio aboout yourself here',
+        name: 'bio',
+      },
+      {
+        type: 'input',
+        message: 'What is your LinkedIn URL?',
+        name: 'LinkedInURL',
+      },
+      {
+        type: 'input',
+        message: 'What is your GitHub URL?',
+        name: 'GitHubURL',
+      }
 ];
 
 // function to write README file
@@ -14,7 +39,19 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    inquirer
+    .prompt([
+        questions();
+    ])
+    .then((data) => {
+  
+    const readmeName = `README.md`
 
+    const finalString = generateMarkdown(data);
+
+    writeToFile(readmeName, finalString)
+
+    }
 }
 
 // function call to initialize program
